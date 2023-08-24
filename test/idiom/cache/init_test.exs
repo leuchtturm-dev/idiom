404ea5ff3a6b73ffce62f226a04bca3546e6e999
Christoph Schmatzler <christoph@medium.place>
2023-08-24 21:39:00 +0200

    kill ets after test

--- test/idiom/cache/init_test.exs
+++ test/idiom/cache/init_test.exs
@@ -3,7 +3,15 @@ defmodule Idiom.Cache.InitTest do
   alias Idiom.Cache
 
   setup do
-    %{default_table_name: Cache.cache_table_name()}
+    default_table_name = Cache.cache_table_name()
+
+    on_exit(fn ->
+      if :ets.info(default_table_name) != :undefined do
+        :ets.delete(default_table_name)
+      end
+    end)
+
+    %{default_table_name: default_table_name}
   end
 
   test "initializes a public ETS with read concurrency table", %{default_table_name: default_table_name} do
